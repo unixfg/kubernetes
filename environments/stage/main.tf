fd0b57f299bce2134bab288ca5513518b6277b93
Ryan Anderson <ryan@joshandryan.net>
2025-08-21 23:47:40 -0500

    Update deprecated configuration comments to reflect new stack structure and deployment instructions

--- environments/stage/main.tf
+++ environments/stage/main.tf
@@ -1,5 +1,5 @@
 // DEPRECATED: This monolithic configuration is no longer used.
 // Use the split stacks instead:
 //   - 1-azure (AKS + AKV/SOPS)
-//   - 2-argocd (ArgoCD)
-// See deploy.sh and README.md in this folder.
+//   - 2-platform (ArgoCD + SOPS Operator)
+// See README.md in this folder for deployment instructions.
